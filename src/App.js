15b085d8649887d0cba4948fa2b847995de95174
Kshitij Kumar <kshitij27kumar@gmail.com>
2022-12-31 17:36:36 +0530

    Text Utils App Completed

--- src/App.js
+++ src/App.js
@@ -25,38 +25,26 @@ function App() {
       setMode('dark')
       document.body.style.backgroundColor = '#042743'
       showAlert('Dark mode has been enabled', 'success')
-      document.title = 'TextUtils - Dark Mode'
-      // setInterval(() => {
-      //   document.title = 'TextUtils is Amazing Mode';
-      // }, 2000);
-      // setInterval(() => {
-      //   document.title = 'Install TextUtils Now';
-      // }, 1500);
     } else {
       setMode('light')
       document.body.style.backgroundColor = 'white'
       showAlert('Light mode has been enabled', 'success')
-      document.title = 'TextUtils - Light Mode'
     }
   }
   return (
     <>
-      {/* <Navbar title="TextUtils" aboutText="About TextUtils" /> */}
-      {/* <Navbar/> */}
       <Router>
         <Navbar title='TextUtils' mode={mode} toggleMode={toggleMode} />
         <Alert alert={alert} />
         <div className='container my-3'>
           <Routes>
-            {/* /users --> Component 1
-        /users/home --> Component 2 */}
-            <Route path='/about' element={<About />}></Route>
+            <Route path='/about' element={<About mode={mode} />}></Route>
             <Route
               path='/'
               element={
                 <TextForm
                   showAlert={showAlert}
-                  heading='Enter the text to analyze below'
+                  heading='Try TextUtils - word counter, character counter, remove extra spaces'
                   mode={mode}
                 />
               }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -25,10 +25,9 @@ export default function Navbar(props) {
         <div className='collapse navbar-collapse' id='navbarSupportedContent'>
           <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
             <li className='nav-item'>
-              <Link className='nav-link active' aria-current='page' to='/'>
+              <Link className='nav-link' aria-current='page' to='/'>
                 Home
               </Link>
-              {/* <a className="nav-link active" aria-current="page" href="#">Home</a> */}
             </li>
             <li className='nav-item'>
               <Link className='nav-link' to='/about'>
@@ -36,11 +35,6 @@ export default function Navbar(props) {
               </Link>
             </li>
           </ul>
-          {/* <form className="d-flex">
-                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                        <button cla
-                        ssName="btn btn-primary" type="submit">Search</button>
-                    </form> */}
           <div
             className={`form-check form-switch text-${
               props.mode === 'light' ? 'dark' : 'light'
@@ -56,7 +50,9 @@ export default function Navbar(props) {
               className='form-check-label'
               htmlFor='flexSwitchCheckDefault'
             >
-              Enable DarkMode
+              {props.mode === 'light'
+                ? 'Enable Dark Mode'
+                : 'Enable Light Mode'}
             </label>
           </div>
         </div>
